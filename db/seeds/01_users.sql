3fabc656bff7b53e3a795cc5d9c25a0e2f40973a
Matija <matija_bozic@yahoo.com>
2022-01-09 08:14:05 +0000

    added seeds & homepage code at server.js

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,6 @@
 -- Users table seeds here (Example)
 INSERT INTO users (name) VALUES ('Alice');
 INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (name) VALUES ('Talia');
+INSERT INTO users (name) VALUES ('Casey');
+INSERT INTO users (name) VALUES ('Matija');
