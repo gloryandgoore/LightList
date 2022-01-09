7f773b4ce23b72bd486aca9cb3ffa09a2b00f4a2
Matija <matija_bozic@yahoo.com>
2022-01-09 08:20:59 +0000

    zz

--- db/seeds/03_messages.sql
+++ db/seeds/03_messages.sql
@@ -1,2 +1,5 @@
-INSERT INTO messages (content, sender_id, recipient_id, sent_at) VALUES ();
+INSERT INTO messages (conversation_id, from_buyer, content) VALUES (1, True, 'Hello, is this vehicle still available?');
+INSERT INTO messages (conversation_id, from_buyer, content) VALUES (1, False, 'Yes, it is available. Would you like a test drive?.');
+INSERT INTO messages (conversation_id, from_buyer, content) VALUES (1, True, 'Sure, lets crash it!');
+INSERT INTO messages (conversation_id, from_buyer, content) VALUES (1, False, 'Okay, meet me on my driveway.');
 
