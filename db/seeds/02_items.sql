a1b6b6cc4ee6c9715fb0c7ebb9d094197c91349f
Talia Aziz Khan <talyaaziz@gmail.com>
2022-01-09 10:49:32 +0000

    update seeds

--- db/seeds/02_items.sql
+++ db/seeds/02_items.sql
@@ -1,20 +1,20 @@
-INSERT INTO items (id, name, seller_id, is_sold, price, description, is_featured, date_posted, image_url)
+INSERT INTO items (id, make, model, seller_id, is_sold, price, description, year, is_featured, date_posted, image_url)
 VALUES
-    (1, 'Ford ',         1, 1, 5000, 'Good condition, 20000 miles,   2002', 0, '2021-07-23', 'temp'),
-    (2, 'Mercedes',      2, 1, 12000, 'well-maintained, 20000 miles, 2001', 1, '2021-07-23', 'temp'),
-    (3, 'Audi',          2, 1, 12000, 'well-maintained, 20000 miles, 2001', 1, '2021-07-23', 'temp'),
-    (4, 'Toyota',        1, 1, 12000, 'well-maintained, 20000 miles, 2001', 1, '2021-07-23', 'temp'),
-    (5, 'Honda',         1, 1, 12000, 'well-maintained, 20000 miles, 2001', 1, '2021-07-23', 'temp'),
-    (6, 'Nissan',        2, 1, 12000, 'well-maintained, 20000 miles, 2001', 1, '2021-07-23', 'temp'),
-    (7, 'Honda',         1, 1, 12000, 'well-maintained, 20000 miles, 2001', 1, '2021-07-23', 'temp'),
-    (8, 'Chevrolet',     2, 1, 12000, 'well-maintained, 20000 miles, 2001', 1, '2021-07-23', 'temp'),
-    (9, 'Tesla',         2, 1, 12000, 'well-maintained, 20000 miles, 2001', 1, '2021-07-23', 'temp'),
-    (10, 'BMW',          1, 1, 12000, 'well-maintained, 20000 miles, 2001', 1, '2021-07-23', 'temp'),
-    (11, 'Ferrari',      2, 1, 12000, 'well-maintained, 20000 miles, 2001', 1, '2021-07-23', 'temp'),
-    (12, 'Mitsubishi',   2, 1, 12000, 'well-maintained, 20000 miles, 2001', 1, '2021-07-23', 'temp'),
-    (13, 'Porsche',      2, 1, 12000, 'well-maintained, 20000 miles, 2001', 1, '2021-07-23', 'temp'),
-    (14, 'Rolls-Royce',  1, 1, 12000, 'well-maintained, 20000 miles, 2001', 1, '2021-07-23', 'temp'),
-    (15, 'Mazda',        1, 1, 12000, 'well-maintained, 20000 miles, 2001', 1, '2021-07-23', 'temp'),
+    (1, 'Ford ', 'Windstar'    	1, 1, 23862, 'Good condition, 11268 km', 2017, 0, '2021-07-23', 'temp'),
+    (2, 'Mercedes', 'Benz'     	2, 1, 66000, 'well-maintained, 20000 miles', 2001, 1, '2021-07-23', 'temp'),
+    (3, 'Audi', 'R8',          	2, 0, 14000, 'Excellent condition, 20000 miles', 2001, 1, '2021-07-23', 'temp'),
+    (4, 'Toyota', 'Corolla',		1, 0, 23241, 'Average-condition', 2001, 1, '2021-07-23', 'temp'),
+    (5, 'Honda', 'Civic'       	1, 1, 9000, 'well-maintained, 20000 miles', 2001, 1, '2021-07-23', 'temp'),
+    (6, 'Nissan', 'GTR',       	2, 1, 21882, 'well-maintained, 37193km', 2018,   1, '2021-07-23', 'temp'),
+    (7, 'Volkswagen', 'Beetle',    	1, 0, 12000, 'well-maintained, 20000 miles', 2001, 1, '2021-07-23', 'temp'),
+    (8, 'Chevrolet', 'ChevroletModel'  2, 0, 9500, 'well-maintained, 20000 miles', 2001, 1, '2021-07-23', 'temp'),
+    (9, 'Tesla', 'X',         	2, 1, 12000, 'well-maintained, 20000 miles', 2020, 1, '2021-07-23', 'temp'),
+    (10, 'BMW', 'BMWModel'    	1, 0, 14000, 'well-maintained, 20000 miles', 2001, 1, '2021-07-23', 'temp'),
+    (11, 'Ferrari', 'FerrariModel',    2, 1, 17000, 'well-maintained, 20000 miles', 2001, 1, '2021-07-23', 'temp'),
+    (12, 'Mitsubishi', 'MitsibusiModel', 2, 0, 7000, 'well-maintained, 20000 miles', 2001, 1, '2021-07-23', 'temp'),
+    (13, 'Porsche', 'PorscheModel',      2, 0, 17000, 'well-maintained, 20000 miles', 2001, 1, '2021-07-23', 'temp'),
+    (14, 'Rolls-Royce', 'RollsModel',  1, 0, 19000, 'well-maintained, 20000 miles', 2001, 1, '2021-07-23', 'temp'),
+    (15, 'Mazda', 'MazdaModel',        1, 1, 6000, 'well-maintained, 20000 miles' 2004, 1, '2021-07-23', 'temp');
     
     
     
--- db/seeds/03_messages.sql
+++ db/seeds/03_messages.sql
@@ -1,8 +1,16 @@
 INSERT INTO messages (content, sender_id, recipient_id, sent_at)
 VALUES
     ('Sill available?', 1, 2, '2021-09-05 18:19:03'),
-    ('Yes. Price is firm.', 2, 1, '2021-09-05 19:19:03'),
-    ('I will take it.', 1, 2, '2021-09-05 21:19:03'),
+    ('Yes. Price is firm.', 2, 1, '2021-09-05 19:20:07'),
+    ('I will take it.', 1, 2, '2021-09-05 19:21:12'),
     ('Hi. Still for sale? I would like to see in person.', 3, 4, '2021-12-01 08:30:50'),
     ('Sure. How about tomorrow at the mall?', 4, 3, '2021-12-01 08:45:05'),
-    ('That works. See you then.', 3, 4, '2021-12-01 09:30:35');
+    ('That works. See you then.', 3, 4, '2021-12-01 08:50:35');
+    ('Hi! is the price negotiable?', 5, 6, '2021-10-06 09:30:35');
+    ('No, Sorry, its fixed', 6, 5, '2021-10-06 09:32:35');
+    ('Has there ever been a dent on the car?', 7, 8, '2021-04-23 09:45:45');
+    ('No, its clean.', 8, 7, '2021-04-23 09:50:39');
+    ('Great!, Can I see it tomorrow?', 7, 8, '2021-04-23 10:10:35');
+    ('How about Sunday', 8, 7, '2021-04-23 10:30:35');
+    ('Ok sounds good! What place?', 7, 8, '2021-04-23 10:40:35');
+    ('Hi Where is it located?', 9, 10, '2021-11-29 17:35:25'),
