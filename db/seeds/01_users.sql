7585f1df936a54afd196850562bc4e4f5025e5c3
Matija <matija_bozic@yahoo.com>
2022-01-09 08:22:42 +0000

    trying to fix commit to seeds

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,6 +1,56 @@
--- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
-INSERT INTO users (name) VALUES ('Talia');
-INSERT INTO users (name) VALUES ('Casey');
-INSERT INTO users (name) VALUES ('Matija');
+ INSERT INTO users (
+    name, email, password, isAdmin)
+    VALUES (
+    'Iva Harrison', 'allisonjackson@mail.com', 'password', true);
+    INSERT INTO users (
+    name, email, password, isAdmin)
+    VALUES (
+    'Lloyd Jefferson', 'asherpoole@gmx.com', 'password', true);
+    INSERT INTO users (
+    name, email, password, isAdmin)
+    VALUES (
+    'Dale Coleman', 'michaelgray@mail.com', 'password', true);
+    INSERT INTO users (
+    name, email, password, isAdmin)
+    VALUES (
+    'Alejandro Osborne', 'ariaatkinson@outlook.com', 'password', true);
+    INSERT INTO users (
+    name, email, password, isAdmin)
+    VALUES (
+    'Nell Medina', 'juliansantos@aol.com', 'password', false);
+    INSERT INTO users (
+    name, email, password, isAdmin)
+    VALUES (
+    'Estelle Walsh', 'elistanton@yahoo.com', 'password', false);
+    INSERT INTO users (
+    name, email, password, isAdmin)
+    VALUES (
+    'Herbert Graves', 'emilyowen@live.com', 'password', false);
+    INSERT INTO users (
+    name, email, password, isAdmin)
+    VALUES (
+    'John Stevens', 'charliebattle@yahoo.com', 'password', false);
+    INSERT INTO users (
+    name, email, password, isAdmin)
+    VALUES (
+    'Isabelle Robbins', 'miasutton@aol.com', 'password', false);
+    INSERT INTO users (
+    name, email, password, isAdmin)
+    VALUES (
+    'Jerome Wright', 'bellaanthony@gmail.com', 'password', false);
+    INSERT INTO users (
+    name, email, password, isAdmin)
+    VALUES (
+    'May Barrett', 'gabriellabarrera@aol.com', 'password', false);
+    INSERT INTO users (
+    name, email, password, isAdmin)
+    VALUES (
+    'Christine Buchanan', 'nathannguyen@ymail.com', 'password', false);
+    INSERT INTO users (
+    name, email, password, isAdmin)
+    VALUES (
+    'Oscar Little', 'alainamcfarland@hotmail.com', 'password', false);
+    INSERT INTO users (
+    name, email, password, isAdmin)
+    VALUES (
+    'Mabelle Hughes', 'jordansears@outlook.com', 'password', false);
