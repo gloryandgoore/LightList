669e347fac799a0f057e5d0027e2951fa6ed05ec
Caseyann <goore_44@hotmail.ca>
2022-01-09 14:53:43 -0500

    clean up some files

--- db/seeds/02_messages.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO messages (conversation_id, from_buyer, content) VALUES (1, True, 'Hello, is this vehicle still available?');
-INSERT INTO messages (conversation_id, from_buyer, content) VALUES (1, False, 'Yes, it is available. Would you like a test drive?.');
-INSERT INTO messages (conversation_id, from_buyer, content) VALUES (1, True, 'Sure, lets crash it!');
-INSERT INTO messages (conversation_id, from_buyer, content) VALUES (1, False, 'Okay, meet me on my driveway.');
