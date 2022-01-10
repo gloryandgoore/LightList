b91fea1e2c64a50786705a115798ad853318756a
Caseyann <goore_44@hotmail.ca>
2022-01-09 22:03:44 -0500

    implement additional routes

---
+++ routes/items.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const router  = express.Router();
+
+module.exports = (db) => {
+  router.get("/:id", (req, res) => {
+
+    const id = req.params.id;
+    const sql = `SELECT * FROM items WHERE id = $1`
+    console.log(id)
+
+    db.query(sql, [id])
+      .then(data => {
+        const templateVars = {item: data.rows[0], user_id: req.session.user_id};
+        console.log("User id", templateVars.user_id, "Item seller id", templateVars.item.seller_id)
+        res.render("item", templateVars);
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+
+  router.post("/sold/:id", (req, res) => {
+    const params = req.params;
+    const sql = `UPDATE items
+    SET is_sold = true
+    WHERE id = $1;`
+    console.log(params)
+    db.query(sql, [params.id])
+      .then(data => {
+        res.redirect("/")
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+
+  return router;
+};
