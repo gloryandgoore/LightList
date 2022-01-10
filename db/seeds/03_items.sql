7a93f488155213415ef89ec89ab09cebc453acc8
Caseyann <goore_44@hotmail.ca>
2022-01-09 19:48:36 -0500

    implement seeds and tables

---
+++ db/seeds/03_items.sql
@@ -0,0 +1,13 @@
+INSERT INTO items (seller_id, price, name, description, category_id, image_url, is_available) VALUES (1, 10000000, 'Ford F-150', 'Almost new Ford F-150', 1, 'https://www.thedrive.com/content/2019/03/2019-Ford-F-150-Raptor-Top-Image.jpg?quality=85', true);
+INSERT INTO items (seller_id, price, name, description, category_id, image_url, is_available) VALUES (1, 150000, 'Blood-Pressure Machine', 'Test Blood-pressure at home', 2, 'https://www.tlbox.com/wp-content/uploads/2014/01/Omron-Omron-Series-Upper-Arm-Blood-Pressure-Monitor-1024x796.jpg', true);
+INSERT INTO items (seller_id, price, name, description, category_id, image_url, is_available) VALUES (1, 10000, 'Cool Dog' , 'Cool dog, just poops everywhere', 3, 'https://i.ebayimg.com/00/s/MTAyNFg3Njg=/z/AFMAAOSwAbhahvf0/$_86.JPG', false);
+INSERT INTO items (seller_id, price, name, description, category_id, image_url, is_available) VALUES (3, 100, 'Free dresser', 'Free if you can pick it up', 4, 'https://www.amishfurnishings.com/wp-content/uploads/2019/05/20190501_123432.jpg', true);
+INSERT INTO items (seller_id, price, name, description, category_id, image_url, is_available) VALUES (1, 305000, 'Gaming Setup', 'Includes chair and desk.', 5, 'https://tse1.mm.bing.net/th?id=OIP.cdJBB9txEuY4Iq9oAl3VQwHaFj&pid=Api&P=0&w=217&h=163', false);
+INSERT INTO items (seller_id, price, name, description, category_id, image_url, is_available) VALUES (1, 250000000, 'Beautiful 2 bedroom in Toronto', '2 bedrooms, 1 bathroom basement apartment.', 6, 'https://media-cdn.tripadvisor.com/media/vr-splice-j/05/73/32/0d.jpg', true);
+INSERT INTO items (seller_id, price, name, description, category_id, image_url, is_available) VALUES (3, 7000, 'Custom Crocs', 'Hand painted custom crocs, size 6', 7, 'https://tse4.mm.bing.net/th?id=OIP.vLVODnems81fcuNDJYzsswHaHa&pid=Api&P=0&w=163&h=163', false);
+INSERT INTO items (seller_id, price, name, description, category_id, image_url, is_available) VALUES (3, 3000, 'PS-5', 'Used, $600', 8, 'https://www.adspakistan.net/wp-content/uploads/2020/10/s-l1600-1.jpg', false);
+INSERT INTO items (seller_id, price, name, description, category_id, image_url, is_available) VALUES (3, 30000, 'Ninja Airfryer', 'New, Airfryer', 9, 'https://photos.offerup.com/Ygo8Sqt8Eyi5p2hukEgGjaTs5I8=/600x800/46d1/46d11d47fd3847dd965ebcf112ec817e.jpg', true);
+INSERT INTO items (seller_id, price, name, description, category_id, image_url, is_available) VALUES (3, 2000, 'Trampoline', 'Brand new', 10, 'https://tse1.mm.bing.net/th?id=OIP.dh-3YQje0tvjEGsaCGzoAAHaGH&pid=Api&P=0&w=199&h=164', true);
+
+
+
