d1cae7a24724c0f8e92139a1006fa825111b0593
Talia Aziz Khan <talyaaziz@gmail.com>
2022-01-11 08:51:09 +0000

    front end initialize

--- server.js
+++ server.js
@@ -24,14 +24,14 @@ app.use(morgan("dev"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
-app.use(
-  "/styles",
-  sassMiddleware({
-    source: __dirname + "/styles",
-    destination: __dirname + "/public/styles",
-    isSass: false, // false => scss, true => sass
-  })
-);
+// app.use(
+//   "/styles",
+//   sassMiddleware({
+//     source: __dirname + "/styles",
+//     destination: __dirname + "/public/styles",
+//     isSass: false, // false => scss, true => sass
+//   })
+// );
 
 app.use(express.static("public"));
 
@@ -39,13 +39,13 @@ app.use(express.static("public"));
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
 const widgetsRoutes = require("./routes/widgets");
-const itemRoutes = require("./routes/items");
+// const itemRoutes = require("./routes/items");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
 app.use("/api/widgets", widgetsRoutes(db));
-app.use("/", itemRoutes(db));
+// app.use("/", itemRoutes(db));
 // Note: mount other resources here, using the same pattern above
 
 // Home page
